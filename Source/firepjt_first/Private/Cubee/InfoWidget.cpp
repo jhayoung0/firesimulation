76535d00b668833667cc4ce9b6275b5b1c878156
cubee021 <98734049+cubee021@users.noreply.github.com>
2025-10-09 18:51:52 +0900

    20251009 정보성 UI 추가

--- Source/firepjt_first/Private/Cubee/InfoWidget.cpp
+++ Source/firepjt_first/Private/Cubee/InfoWidget.cpp
@@ -3,3 +3,62 @@
 
 #include "Cubee/InfoWidget.h"
 
+#include "Components/TextBlock.h"
+
+
+void UInfoWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	InitializeTextBlocks();
+	SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UInfoWidget::SetInfoDataAsset(UInfoDataAsset* NewDataAsset)
+{
+	InfoDataAsset = NewDataAsset;
+}
+
+void UInfoWidget::ViewInfoWidget(int32 Idx)
+{
+	InitializeTextBlocks();
+	SetVisibility(ESlateVisibility::Visible);
+
+	SetInfoTextFromIndex(Idx);
+
+	if (FadeIn)
+	{
+		PlayAnimation(FadeIn);
+	}
+}
+
+void UInfoWidget::RemoveInfoWidget()
+{
+	SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UInfoWidget::SetInfoTextFromIndex(int32 Index)
+{
+	if (!InfoDataAsset)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No DataAsset found"));
+		return;
+	}
+
+	if (!InfoDataAsset->InfoTexts.IsValidIndex(Index))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Invalid Index Value"));
+		return;
+	}
+
+	const FInfoTextEntry Entry = InfoDataAsset->InfoTexts[Index];
+
+	Txt_Title->SetText(Entry.Title);
+	Txt_Description->SetText(Entry.Description);
+}
+
+void UInfoWidget::InitializeTextBlocks()
+{
+	Txt_Title->SetText(FText::GetEmpty());
+	Txt_Description->SetText(FText::GetEmpty());
+}
