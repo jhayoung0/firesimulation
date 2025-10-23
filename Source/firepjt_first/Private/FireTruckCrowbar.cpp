b965778028b049a651bf7a3fe3fe8a1637affdb6
CookieAndJuice <cradiacradia@gmail.com>
2025-10-23 22:25:09 +0900

    [temp] 소방관 소방차&소방차 속 상호작용 물건(소방 호스, 쇠지렛대) 구현 중
    - 맞는 방법인지 잘 모르겠다

---
+++ Source/firepjt_first/Private/FireTruckCrowbar.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireTruckCrowbar.h"
+
+
+// Sets default values
+AFireTruckCrowbar::AFireTruckCrowbar()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CrowbarComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CrowbarComp"));
+	ConstructorHelpers::FObjectFinder<UStaticMesh> CrowbarRef(TEXT("/Script/Engine.StaticMesh'/Game/Fab/Crowbar_Low-poly/crowbar/StaticMeshes/crowbar.crowbar'"));
+	if (CrowbarRef.Succeeded())
+	{
+		CrowbarComp->SetStaticMesh(CrowbarRef.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AFireTruckCrowbar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/firepjt_first/Private/FireTruckFireHose.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireTruckFireHose.h"
+
+
+// Sets default values
+AFireTruckFireHose::AFireTruckFireHose()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	FireHoseComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FireHoseComp"));
+	ConstructorHelpers::FObjectFinder<UStaticMesh> fireHoseRef(TEXT("/Script/Engine.StaticMesh'/Game/CustomContents/Fireman/FireTruck/emergency_fire_hose_9_mb/StaticMeshes/emergency_fire_hose_9_mb.emergency_fire_hose_9_mb'"));
+	if (fireHoseRef.Succeeded())
+	{
+		FireHoseComp->SetStaticMesh(fireHoseRef.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AFireTruckFireHose::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
